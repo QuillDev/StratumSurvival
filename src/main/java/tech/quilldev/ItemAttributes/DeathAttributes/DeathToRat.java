680686254fb791f454d3db67771743fd12f57e32
Robert Brunson <siinspam@gmail.com>
2021-05-01 02:10:39 -0400

    latest

--- src/main/java/tech/quilldev/ItemAttributes/DeathAttributes/DeathToRat.java
+++ src/main/java/tech/quilldev/ItemAttributes/DeathAttributes/DeathToRat.java
@@ -2,8 +2,10 @@ package tech.quilldev.ItemAttributes.DeathAttributes;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Chicken;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Wolf;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 public class DeathToRat extends DeathAttribute {
@@ -14,9 +16,15 @@ public class DeathToRat extends DeathAttribute {
 
     @Override
     public void execute(EntityDeathEvent event) {
+
+        final var entity = event.getEntity();
         //Spawn a rat at the location where the entity died
-        var chicken = (Chicken) event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), EntityType.CHICKEN);
-        chicken.setCustomName("rat");
+        var chicken = (Chicken) entity.getWorld().spawnEntity(entity.getLocation(), EntityType.CHICKEN);
+        var wolf = (Wolf) entity.getWorld().spawnEntity(entity.getLocation(), EntityType.WOLF);
+
+        final var killer = entity.getKiller();
+        if (killer == null) return;
+        chicken.setCustomName(killer.getName() + "'s Rat");
         chicken.setCustomNameVisible(false);
         chicken.setBaby();
         chicken.setAI(true);
