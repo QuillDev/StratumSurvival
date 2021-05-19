f5304c724827da76855ddac882d55e1b178626cf
Robert Brunson <siinspam@gmail.com>
2021-05-19 19:55:38 -0400

    talking npcs

---
+++ src/main/java/tech/quilldev/NPCManager/ChatNPC/ChatNPC.java
@@ -0,0 +1,60 @@
+package tech.quilldev.NPCManager.ChatNPC;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ChatNPC {
+
+    private static final Random rand = new Random();
+
+    private final Villager villager;
+    private final ArrayList<Component> chatLines = new ArrayList<>();
+    private final Plugin plugin;
+    private int chatEventId = -1;
+
+    public ChatNPC(Villager villager, Plugin plugin) {
+        this.villager = villager;
+        this.plugin = plugin;
+        this.chatEventId = startChatting();
+    }
+
+    /**
+     * Chat the villager line to nearby players
+     */
+    public void chatNearbyPlayers() {
+        if (chatLines.size() == 0) return;
+        final var line = chatLines.get(rand.nextInt(chatLines.size()));
+        villager.getNearbyEntities(5, 5, 5)
+                .stream()
+                .filter(entity -> entity instanceof Player)
+                .map(entity -> (Player) entity)
+                .forEach(player -> player.sendMessage(
+                        Component.text(villager.getCustomName())
+                                .append(Component.text(":"))
+                                .append(Component.space())
+                                .append(line)
+                ));
+    }
+
+    public void addLine(Component component) {
+        chatLines.add(component);
+    }
+
+    public int startChatting() {
+        if (this.chatEventId == -1) {
+            this.chatEventId = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, this::chatNearbyPlayers, 80L, 100L);
+        }
+        return -1;
+    }
+
+    public void stopChatting() {
+        Bukkit.getScheduler().cancelTask(this.chatEventId);
+        this.chatEventId = -1;
+    }
+}
