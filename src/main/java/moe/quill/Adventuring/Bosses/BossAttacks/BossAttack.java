55fc5cc15c0c1c6780a8856475a88f57f7478367
HireMeDeveloper <jefflamb9@gmail.com>
2021-05-27 23:16:36 -0700

    added some boss attacks that use boss effects

---
+++ src/main/java/moe/quill/Adventuring/Bosses/BossAttacks/BossAttack.java
@@ -0,0 +1,7 @@
+package moe.quill.Adventuring.Bosses.BossAttacks;
+
+import org.bukkit.entity.LivingEntity;
+
+public abstract class BossAttack {
+    public abstract void execute(LivingEntity boss);
+}
---
+++ src/main/java/moe/quill/Adventuring/Bosses/BossAttacks/SpawnZombiesAttack.java
@@ -0,0 +1,16 @@
+package moe.quill.Adventuring.Bosses.BossAttacks;
+
+import moe.quill.Adventuring.Bosses.BossEffects.PotionBasedBossEffect;
+import moe.quill.Adventuring.Bosses.BossEffects.SpawnEnemiesBossEffect;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.potion.PotionEffectType;
+
+public class SpawnZombiesAttack extends BossAttack{
+    private static final SpawnEnemiesBossEffect BossEffect = new SpawnEnemiesBossEffect();
+
+    @Override
+    public void execute(LivingEntity boss) {
+        BossEffect.execute(boss, EntityType.ZOMBIE, 1);
+    }
+}
